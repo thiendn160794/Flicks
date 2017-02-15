5c9b683091e2b399fab3a648307c634258f89c16
THIENDNSE61357\thiendn <hoinganmotti@gmail.com>
2017-02-15 09:45:54 +0700

    Move to package

--- app/src/main/java/com/thiendn/coderschool/flicks/activity/MainActivity.java
+++ app/src/main/java/com/thiendn/coderschool/flicks/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.thiendn.coderschool.flicks;
+package com.thiendn.coderschool.flicks.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.thiendn.coderschool.flicks.R;
 
 public class MainActivity extends AppCompatActivity {
 
